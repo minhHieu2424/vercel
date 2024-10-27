d0674b27ddf05b5b50761a2717c9da2b74a3413d
minhHieu2424 <kingbobbb024@gmail.com>
2024-10-27 23:11:43 +0700

    last commit

--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,17 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.GRPC,
     options: {
-      package: ['product', 'category', 'cart','user','order','car_items'],
+      package: ['product', 'category', 'cart', 'user', 'order', 'car_items'],
       protoPath: [
-        join(__dirname, 'proto/product.proto'),  
-        join(__dirname, 'proto/category.proto'),
-        join(__dirname, 'proto/cart.proto'),
-        join(__dirname, 'proto/user.proto'),
-        join(__dirname, 'proto/order.proto'),
-        join(__dirname, 'proto/cart-item.proto'),
+        join(__dirname, '../proto/product.proto'),  
+        join(__dirname, '../proto/category.proto'),
+        join(__dirname, '../proto/cart.proto'),
+        join(__dirname, '../proto/user.proto'),
+        join(__dirname, '../proto/order.proto'),
+        join(__dirname, '../proto/cart-item.proto'),
       ],
-      url: 'localhost:50051',
-        },
+      url: '0.0.0.0:' + (process.env.PORT || '50051'),
+    },
   });
 
   await app.listen();
